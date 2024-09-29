fd6514f01a44847dd8fed9fdb2b1581bbe14599b
macrespo42 <macrespo@student.42.fr>
2024-09-29 22:19:33 +0200

    feat: Draw cell

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-from window import Line, Point, Window
+from cell import Cell
+from window import Window
 
 
 def main() -> None:
     win = Window(800, 600)
-    line = Line(Point(100, 100), Point(500, 500))
-    win.draw_line(line)
+    first_cell = Cell(win)
+    first_cell.draw(100, 100, 500, 500)
     win.wait_for_close()
 
 
