912377467d14cccf10c6a26e5ec0f70f68c30830
macrespo42 <macrespo@student.42.fr>
2024-10-02 22:29:13 +0200

    breaking walls in the maze

--- cell.py
+++ cell.py
@@ -15,23 +15,35 @@ class Cell:
 
     def draw(self, x1: int, y1: int, x2: int, y2: int) -> None:
         if self._win is None:
-            return
+                return
         self._x1 = x1
-        self._y1 = y1
         self._x2 = x2
+        self._y1 = y1
         self._y2 = y2
-        if self.has_top_wall:
-            line = Line(Point(x1, y1), Point(x2, y1))
-            self._win.draw_line(line)
-        if self.has_bot_wall:
-            line = Line(Point(x1, y2), Point(x2, y2))
-            self._win.draw_line(line)
         if self.has_left_wall:
             line = Line(Point(x1, y1), Point(x1, y2))
             self._win.draw_line(line)
+        else:
+            line = Line(Point(x1, y1), Point(x1, y2))
+            self._win.draw_line(line, "white")
+        if self.has_top_wall:
+            line = Line(Point(x1, y1), Point(x2, y1))
+            self._win.draw_line(line)
+        else:
+            line = Line(Point(x1, y1), Point(x2, y1))
+            self._win.draw_line(line, "white")
         if self.has_right_wall:
             line = Line(Point(x2, y1), Point(x2, y2))
             self._win.draw_line(line)
+        else:
+            line = Line(Point(x2, y1), Point(x2, y2))
+            self._win.draw_line(line, "white")
+        if self.has_bot_wall:
+            line = Line(Point(x1, y2), Point(x2, y2))
+            self._win.draw_line(line)
+        else:
+            line = Line(Point(x1, y2), Point(x2, y2))
+            self._win.draw_line(line, "white")
 
     def draw_move(self, to_cell: object, undo=False):
         half_length = abs(self._x2 - self._x1) // 2
